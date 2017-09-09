1c641f309d356f12ff6c4c2d9275f094cf0958be
Jon Lauridsen <mail@jonlauridsen.com>
2017-09-10 00:15:38 +0100

    Enable move-of-sibling-files logic

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,35 +3,50 @@
 const assertDir = require('assert-dir-equal')
 const Metalsmith = require('metalsmith')
 const path = require('path')
+const merge = require('lodash.merge')
 
-const helpers = require('./helpers')
 const blog = require('../lib/index')
 
 describe('metalsmith-blog', function () {
-  for (let [name, dir] of helpers.walkFixtures('./fixtures')) {
-    describe(name, function () {  // eslint-disable-line no-loop-func
-      it(name, function () {
-        return assertMetalsmithBuildEquals(
-          {
-            layout: {
-              default: 'default.pug',
-              directory: '.',
-              inplace: require('nunjucks-tags-typography')
-            }
-          },
-          path.join(dir),
-          path.join(dir, 'expected')
-        )
-      })
+  it('basic', function () {
+    return assertMetalsmithBuildEquals(this.test.title)
+  })
+
+  it('code', function () {
+    return assertMetalsmithBuildEquals(this.test.title)
+  })
+
+  it('codequotes', function () {
+    return assertMetalsmithBuildEquals(this.test.title)
+  })
+
+  it('move', function () {
+    return assertMetalsmithBuildEquals(this.test.title, {
+      layout: {directory: 'move/templates'},
+      pattern: 'foo',
+      sources: ['**/*']
     })
-  }
+  })
+
+  it('quotes', function () {
+    return assertMetalsmithBuildEquals(this.test.title)
+  })
 })
 
-const assertMetalsmithBuildEquals = function (opts, dir, expected) {
+const assertMetalsmithBuildEquals = function (fixture, opts) {
+  const dir = path.resolve(path.join('test', 'fixtures', fixture))
+  opts = merge({
+    layout: {
+      default: 'default.pug',
+      directory: '.',
+      inplace: require('nunjucks-tags-typography')
+    }
+  }, opts)
+
   let dst = path.join(dir, 'build')
   return buildMetalsmith(path.join(dir, 'source'), dst, opts)
     .then(function () {
-      assertDir(dst, expected, {filter: () => true})
+      assertDir(dst, path.join(dir, 'expected'), {filter: () => true})
     })
 }
 
